9a8d3fd70c2721bbe9c2b33d6c7d5825e4495a45
pdiez20252 <80834118+pdiez20252@users.noreply.github.com>
2021-07-15 21:07:19 +0200

    Add files via upload

---
+++ src/Servidor/Partida.java
@@ -0,0 +1,12 @@
+package Servidor;
+
+public class Partida {
+    private Usuario admin;
+    private Usuario noadmin;
+   
+    
+    public Partida(Usuario admin, Usuario noadmin) {
+        this.admin = admin;
+        this.noadmin = admin;
+    }
+}
