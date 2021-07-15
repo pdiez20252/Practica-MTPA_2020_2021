1f25dd8069eaf4134d19b7bc79fa57f9a36e98fb
pdiez20252 <80834118+pdiez20252@users.noreply.github.com>
2021-07-15 02:53:22 +0200

    Update Menu.java

--- src/Servidor/Menu.java
+++ src/Servidor/Menu.java
@@ -1,19 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    CREADO CON EL PLUGIN JFRAME INCORPORADO EN NETBEANS
  */
 package Servidor;
 
-/**
- *
- * @author Pablo
- */
 public class Menu extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Menu
-     */
     public Menu() {
         initComponents();
     }
