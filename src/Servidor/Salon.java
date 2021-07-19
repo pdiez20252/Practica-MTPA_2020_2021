0f05acfd0419f240a88634e9464d7357197489b8
pdiez20252 <80834118+pdiez20252@users.noreply.github.com>
2021-07-19 08:52:46 +0200

    Delete src/Servidor directory

--- src/Servidor/Salon.java
+++
@@ -1,9 +0,0 @@
-package Servidor;
-
-public abstract class Salon {
-    
-    
-    public abstract String getPalabraRandom();
-    public abstract int comprobarLetra();
-    //en los salones hay partidas, hasta 3 partidas
-}
