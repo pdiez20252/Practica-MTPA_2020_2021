3c199dfc2b46784d4ab3b4082d6e0858ea6e116d
pdiez20252 <80834118+pdiez20252@users.noreply.github.com>
2021-07-23 17:53:40 +0200

    Add files via upload

--- src/main/java/com/mycompany/mtpa/cliente/vista/Registro.java
+++ src/main/java/com/mycompany/mtpa/cliente/vista/Registro.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mtpa.cliente.vista;
 
 import java.awt.GridLayout;
@@ -23,15 +18,16 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 
-public class Login extends JFrame{
+public class Registro extends JFrame{
     
+    private JTextField txtMail;
     private JTextField txtUsuario;
     private JPasswordField txtPass;
     private Socket socketCliente;
     private PrintWriter salida;
     private Scanner entrada;
 
-    public Login(){
+    public Registro(){
         super();
         try {
             socketCliente = new Socket("localhost", 1009);
@@ -45,7 +41,7 @@ public class Login extends JFrame{
     }
 
 private JPanel panelPrincipal(){
-    JPanel panel = new JPanel(new GridLayout(3,2));
+    JPanel panel = new JPanel(new GridLayout(4,0));
     
     panel.add(new JLabel("Usuario:"));
     txtUsuario = new JTextField();
@@ -55,56 +51,43 @@ private JPanel panelPrincipal(){
     txtPass.setColumns(20);
     panel.add(new JLabel("Contraseña:"));
     panel.add(txtPass);
-    JButton btnIniciarSesion = new JButton("Iniciar Sesión");
-    btnIniciarSesion.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent arg0) {
-            login();
-        }
-    });
-    panel.add(btnIniciarSesion);
-    JButton btnCrearCuenta = new JButton("Quiero una cuenta nueva");
+    panel.add(new JLabel("Mail:"));
+    txtMail = new JTextField();
+    txtMail.setColumns(20);
+    panel.add(txtMail);
+    JButton btnCrearCuenta = new JButton("Registrar");
     btnCrearCuenta.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent arg0) {
-            new Registro().setVisible(true);
-            setVisible(false);
-            //registrar();
+            registrar();
         }
     });
     panel.add(btnCrearCuenta);
     return panel;
  
 }
-
-private void login(){
-    String usuario = txtUsuario.getText();
-    String pass = txtPass.getText();
-    if(usuario.isEmpty() || pass.isEmpty()){
-        JOptionPane.showMessageDialog(null, "El usuario y la contraseña no pueden estar vacíos",  "Error",JOptionPane.ERROR_MESSAGE);
-
+private void registrar(){
+      String usuario = txtUsuario.getText();
+      String pass = txtPass.getText();
+      String mail = txtMail.getText();
+    if(usuario.isEmpty() || pass.isEmpty()  || mail.isEmpty()){
+        JOptionPane.showMessageDialog(null, "Porfavor rellene todos los datos",  "Error",JOptionPane.ERROR_MESSAGE);
     }else{
-        //protocolo login:usuario:pass
-        String mensaje = "login:"+usuario+":"+pass+"\n";
+        //protocolo login:usuario:pass:mail, enviar los datos al servidor
+        String mensaje = "registro:"+usuario+":"+pass+":"+mail+"\n";
         salida.write(mensaje);
         salida.flush();
         //leer la respuesta del servidor 
         String respuesta = entrada.nextLine();
         if(respuesta.equals("true")){
-            new Salon().setVisible(true);
+            JOptionPane.showMessageDialog(null, "Registro efectuado con éxito", "Registro", JOptionPane.INFORMATION_MESSAGE);
+            new Login().setVisible(true);
             setVisible(false);
-                    //pasar a la ventana principal
         }else{
-          JOptionPane.showMessageDialog(null, "Usuario o contraseña inválidas",  "Error",JOptionPane.ERROR_MESSAGE);
+          JOptionPane.showMessageDialog(null, "Registro no válido, usuario o correo ya registrados o contraseña demasiado corta (Mínimo 3 caracteres)",  "Error",JOptionPane.ERROR_MESSAGE);
 
         }
     }
 }
-
-
-
-    public static void main(String[] args) {
-        new Login().setVisible(true);
-    }
 
 }
--- src/main/java/com/mycompany/mtpa/servidor/Servidor.java
+++ src/main/java/com/mycompany/mtpa/servidor/Servidor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mtpa.servidor;
 
 import java.io.IOException;
@@ -58,7 +53,7 @@ public class Servidor {
     }
 
     boolean registro(String usuario, String pass) {
-        if(usuarios.containsKey(usuario)){
+        if(usuarios.containsKey(usuario) || pass.length()<4){
             return false;
         }
         usuarios.put(usuario, new Usuario(usuario, pass));
